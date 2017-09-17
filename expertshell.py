dafa65bcb28134217f1ce9e184d1f8c6d933d0e9
Bryan Chen <bc2vf@virginia.edu>
2017-09-16 23:27:22 -0400

    parse logic

--- expertshell.py
+++ expertshell.py
@@ -54,33 +54,8 @@ class Expert(object):
                     self.learnedVars[k] = [v[0], v[1], False]
             self.rootVars[varName][2] = True
 
-    def teach_rule(self, expr, varName):
-        copy = expr
-        copy.replace('&', ' ')
-        copy.replace('|', ' ')
-        copy.replace('!', ' ')
-        copy.replace('(', ' ')
-        copy.replace(')', ' ')
-        variables = copy.split()
-        if not self.learnedVars.get(varName):
-            return False
-        for v in variables:
-            if not self.rootVars.get(v) and not self.learnedVars.get(v):
-                return False
-            elif self.rootVars.get(v):
-                if self.rootVars[v][1]:
-                    expr.replace(v, 'True')
-                else:
-                    expr.replace(v, 'False')
-            elif self.learnedVars.get(v):
-                if self.learnedVars[v][1]:
-                    expr.replace(v, 'True')
-                else:
-                    expr.replace(v, 'False')
-        expr.replace('&', ' and ')
-        expr.replace('|', ' or ')
-        expr.replace('!', ' not ')
-        self.learnedVars[varName] = [self.learnedVars[varName][0], eval(expr)]
+    def teach_rule(self, expr, val):
+        self.learnedVars[val] = [self.learnedVars[val][0], self.parse_expr(expr, val)]
 
     def list_all(self):
         rootVarsStr = 'Root Variables: \n'
@@ -102,13 +77,38 @@ class Expert(object):
         pass
 
     def query(self, expr):
-        pass
+        self.learnedVars[val] = [self.l]
 
     def why(self, question):
         pass
 
     def parse_expr(self, expr):
-
+        copy = expr
+        copy.replace('&', ' ')
+        copy.replace('|', ' ')
+        copy.replace('!', ' ')
+        copy.replace('(', ' ')
+        copy.replace(')', ' ')
+        variables = copy.split()
+        if not self.learnedVars.get(val):
+            return False
+        for v in variables:
+            if not self.rootVars.get(v) and not self.learnedVars.get(v):
+                return False
+            elif self.rootVars.get(v):
+                if self.rootVars[v][1]:
+                    expr.replace(v, 'True')
+                else:
+                    expr.replace(v, 'False')
+            elif self.learnedVars.get(v):
+                if self.learnedVars[v][1]:
+                    expr.replace(v, 'True')
+                else:
+                    expr.replace(v, 'False')
+        expr.replace('&', ' and ')
+        expr.replace('|', ' or ')
+        expr.replace('!', ' not ')
+        return eval(expr)
 
 
 if __name__ == '__main__':
