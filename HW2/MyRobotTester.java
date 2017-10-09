cf134b66397fc9061a79fbbacc9b174a4ff8373f
Hanscat <hanscat@icloud.com>
2017-10-09 15:11:44 -0400

    write up

--- HW2/MyRobotTester.java
+++ HW2/MyRobotTester.java
@@ -4,7 +4,7 @@ public class MyRobotTester {
 
     public static void main(String[] args) {
         try {
-            World myWorld = new World("TestCases/myInputFile4.txt", true);
+            World myWorld = new World("TestCases/myInputFile7.txt", false);
             RobotTraveler robot = new RobotTraveler();
             robot.addToWorld(myWorld);
             myWorld.createGUI(400, 400, 200); // uncomment this and create a GUI; the last parameter is delay in msecs
